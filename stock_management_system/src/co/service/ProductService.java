1844362481aa5f791b376886b513703f6b7ed54c
thathyda <thydatha484@gmail.com>
2023-11-14 11:49:35 +0700

    Initial commit

---
+++ stock_management_system/src/co/service/ProductService.java
@@ -0,0 +1,20 @@
+package co.service;
+
+import co.model.Product;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductService {
+    Product insert(Product product);
+
+    List<Product> select();
+
+    Product selectById(Long id);
+
+    Product updateById( Product product);
+
+    Product deleteById(Long id);
+
+    List<Product> selectByName(String name);
+}
